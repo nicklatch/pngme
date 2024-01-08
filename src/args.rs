919cd85427de93e86a0dafd8f24735de14a95156
Nick <nick.latcham@proton.me>
2024-01-08 13:29:39 -0600

    ..

--- src/args.rs
+++ src/args.rs
@@ -2,8 +2,9 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+/// A cli tool to embed messages into a PNG file
 #[derive(Debug, Parser)]
-#[command(author, version)]
+#[command(author, version, about, long_about = None)]
 pub struct Commands {
     #[clap(subcommand)]
     pub command: PngMeArgs,
@@ -11,9 +12,13 @@ pub struct Commands {
 
 #[derive(Debug, Parser)]
 pub enum PngMeArgs {
+    /// Encodes the message in a specific PNG file
     Encode(EncodeArgs),
+    /// Decodes the message from a specific PNG file
     Decode(DecodeArgs),
+    /// Remove the message from a specific PNG file
     Remove(RemoveArgs),
+    /// Print the PNG chunks that can be searched for messages
     Print(PrintArgs),
 }
 
@@ -27,17 +32,22 @@ pub struct EncodeArgs {
 
 #[derive(Debug, Parser)]
 pub struct DecodeArgs {
-    pub file_path: PathBuf,
+    /// Chunk type
     pub chunk_type: String,
+    /// Input PNG file path
+    pub file_path: PathBuf,
 }
 
 #[derive(Debug, Parser)]
 pub struct RemoveArgs {
-    pub file_path: PathBuf,
+    /// Chunk type
     pub chunk_type: String,
+    /// Input PNG file path
+    pub file_path: PathBuf,
 }
 
 #[derive(Debug, Parser)]
 pub struct PrintArgs {
+    /// Input PNG file path
     pub file_path: PathBuf,
 }
