25fe9b58a4077339a361daff446183b48fa32a6f
Nick Latcham <nick.latcham@proton.me>
2024-01-13 16:46:28 -0600

    ..

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -6,16 +6,11 @@ use std::fmt;
 use std::fmt::Display;
 use std::str::FromStr;
 
-/// A chunk type is a sequence of four bytes. Each byte must be an ASCII
-/// alphabetic character. The case of each character carries a specific meaning.
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct ChunkType {
     bytes: [u8; 4],
 }
 
-// Trys to convert a byte array into a `ChunkType`.
-///
-/// This will return an error if any of the bytes is not an ASCII alphabetic character.
 impl TryFrom<[u8; 4]> for ChunkType {
     type Error = crate::Error;
 
@@ -29,10 +24,6 @@ impl TryFrom<[u8; 4]> for ChunkType {
     }
 }
 
-/// Converts a string into a `ChunkType`.
-///
-/// This will return an error if the string is not exactly four characters long,
-/// or if any of the characters is not an ASCII alphabetic character.
 impl FromStr for ChunkType {
     type Err = crate::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -54,9 +45,6 @@ impl FromStr for ChunkType {
     }
 }
 
-/// Formats a `ChunkType` for display.
-///
-/// This will output the four characters of the chunk type.
 impl Display for ChunkType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for byte in &self.bytes {
@@ -66,79 +54,48 @@ impl Display for ChunkType {
     }
 }
 
-/// Methods for working with a `ChunkType`.
 impl ChunkType {
     pub fn bytes(&self) -> [u8; 4] {
         self.bytes
     }
 
-    /// Returns whether the chunk type is valid.
-    ///
-    /// A chunk type is valid if it is exactly four characters long,
-    /// each character is an ASCII alphabetic character,
-    /// and the reserved bit is valid.
     pub fn is_valid(&self) -> bool {
         (self.bytes.len() == 4)
             && ChunkType::is_reserved_bit_valid(self)
             && ChunkType::all_valid_bytes(self)
     }
 
-    /// Returns whether the chunk type is valid.
-    ///
-    /// A chunk type is valid if it is exactly four characters long,
-    /// each character is an ASCII alphabetic character,
-    /// and the reserved bit is valid.
     pub fn is_valid_byte(byte: u8) -> bool {
         matches!(byte, 65..=90 | 97..=122)
     }
 
-    /// Returns whether all bytes in the chunk type are valid characters.
     pub fn all_valid_bytes(&self) -> bool {
         self.bytes
             .iter()
             .all(|&byte| ChunkType::is_valid_byte(byte))
     }
 
-    // ?Should this just take an enum varient and a bit instead of coupling this function with each consumer fn?
-    // * This is used only in this file by the next four functions, the functions that utilize it are in in others.
-    // * Be sure to track any changes for breakage
-    /// Returns whether the semantic bit of a byte is zero.
-    ///
-    /// The semantic bit is the fifth bit from the right.
     fn semantic_bit_is_zero(bit: u8) -> bool {
         bit & (1 << 5) == 0
     }
 
-    /// Returns whether the chunk type is critical.
-    ///
-    /// A chunk type is critical if the case of the first character is uppercase.
     pub fn is_critical(&self) -> bool {
         Self::semantic_bit_is_zero(self.bytes[0])
     }
 
-    /// Returns whether the chunk type is public.
-    ///
-    /// A chunk type is public if the case of the second character is uppercase.
     pub fn is_public(&self) -> bool {
         Self::semantic_bit_is_zero(self.bytes[1])
     }
 
-    /// Returns whether the reserved bit of the chunk type is valid.
-    ///
-    /// The reserved bit is valid if the case of the third character is uppercase.
     pub fn is_reserved_bit_valid(&self) -> bool {
         Self::semantic_bit_is_zero(self.bytes[2])
     }
 
-    /// Returns whether the chunk type is safe to copy.
-    ///
-    /// A chunk type is safe to copy if the case of the fourth character is lowercase.
     pub fn is_safe_to_copy(&self) -> bool {
         !Self::semantic_bit_is_zero(self.bytes[3])
     }
 }
 
-/// Represents an error that can occur when decoding a chunk type.
 #[derive(Debug)]
 pub enum ChunkTypeDecodeError {
     InvalidByte(u8),
@@ -146,7 +103,6 @@ pub enum ChunkTypeDecodeError {
     UnkownError,
 }
 
-/// Formats a `ChunkTypeDecodeError` for display.
 impl fmt::Display for ChunkTypeDecodeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -157,7 +113,6 @@ impl fmt::Display for ChunkTypeDecodeError {
     }
 }
 
-/// Allows a `ChunkTypeDecodeError` to be treated as a standard error.
 impl Error for ChunkTypeDecodeError {}
 
 // ----------TESTS-------------//
